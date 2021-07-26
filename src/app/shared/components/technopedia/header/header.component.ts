05e557e1f08f2c4dfbdd64249bb5796499bd2a54
Thangavel Ayyasamy <aagfdzz@mmm.com>
2021-07-26 18:00:19 +0530

    Added Logo and Media Queries

--- src/app/shared/components/technopedia/header/header.component.ts
+++ src/app/shared/components/technopedia/header/header.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   title:string = "Technopedia";
+  languageDisplay: boolean = false;
 
   constructor() { }
 
