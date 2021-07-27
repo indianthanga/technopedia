d15c80fb63304c9a660e3cfd4e3038f0ff6f0843
Thangavel Ayyasamy <aagfdzz@mmm.com>
2021-07-27 14:50:17 +0530

    Added Technologies Stack

--- src/app/shared/components/technopedia/header/header.component.ts
+++ src/app/shared/components/technopedia/header/header.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { ElementRef, Renderer2 } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  title:string = "Technopedia";
+  title: string = 'Technopedia';
   languageDisplay: boolean = false;
+  authorDisplay: boolean = false;
+  @ViewChild('languageDropDown') languageDropDown!: ElementRef;
+  @ViewChild('authorDropDown') authorDropDown!: ElementRef;
 
-  constructor() { }
+  constructor(private renderer: Renderer2) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/technopedia/home/home.component.ts
+++ src/app/technopedia/home/home.component.ts
@@ -3,13 +3,47 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  technologiesStack: any = [];
+  constructor() {
+    this.technologiesStack = [
+      [
+        {
+          name: 'General',
+        },
+        {
+          name: 'Programming',
+        },
+        {
+          name: 'Human Anatomy',
+        },
+      ],
+      [
+        {
+          name: 'History & Facts',
+        },
+        {
+          name: 'Sports',
+        },
+        {
+          name: 'Gaming',
+        },
+      ],
+      [
+        {
+          name: 'New Born',
+        },
+        {
+          name: 'Electronics',
+        },
+        {
+          name: 'Science',
+        },
+      ],
+    ];
   }
 
+  ngOnInit(): void {}
 }
